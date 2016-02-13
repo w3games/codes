cb620b1ffa39bdbd9b432e1a2ea49bdee62a5454
leaf <leaf@localhost.localdomain>
2016-02-13 13:59:11 +0900

    modified:   script/bookmarks-keywords+title-generator.fish

--- script/bookmarks-keywords+title-generator.fish
+++ script/bookmarks-keywords+title-generator.fish
@@ -5,16 +5,17 @@
 grep \<DD\> ~/Documents/homepage/bookmarks.html \
 | sed -e 's/^[ ]*//g' \
 | sed -e 's/^<DD>/・/g' \
-| sort -u > ~/Documents/homepage/bookmarks-title.txt
+| sort -u >> ~/Documents/homepage/bookmarks-title.txt
 
+echo "[Keywords]" > ~/Documents/homepage/bookmarks-keywords.txt
 grep SHORTCUTURL ~/Documents/homepage/bookmarks.html \
 | sed -e 's/.*SHORTCUTURL=\"\(.*\).*$/\1/' \
 | sed -e 's/<\/A>$//g' \
 | sed -e 's/^/・/g' \
-| sort -u > ~/Documents/homepage/bookmarks-keywords.txt
+| sort -u >> ~/Documents/homepage/bookmarks-keywords.txt
 
 echo >> ~/Documents/homepage/bookmarks-keywords.txt
-echo "・[Titles]" >> ~/Documents/homepage/bookmarks-keywords.txt
+echo "[Titles]" >> ~/Documents/homepage/bookmarks-keywords.txt
 
 cat ~/Documents/homepage/bookmarks-title.txt >> ~/Documents/homepage/bookmarks-keywords.txt 
 
