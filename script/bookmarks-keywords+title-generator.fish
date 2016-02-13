ed6f524a5ba20e373b84d0c1f57badcae64cc876
leaf <leaf@localhost.localdomain>
2016-02-13 12:52:49 +0900

    add mail-client/thunderbird/thunderbird-38.6.0.ebuild add script/bookmarks-keywords+title-generator.fish

---
+++ script/bookmarks-keywords+title-generator.fish
@@ -0,0 +1,21 @@
+#!/bin/fish
+#
+# bookmarks keywords+title generator
+
+grep \<DD\> ~/Documents/homepage/bookmarks.html \
+| sed -e 's/^[ ]*//g' \
+| sed -e 's/^<DD>/・/g' \
+| sort -u > ~/Documents/homepage/bookmarks-title.txt
+
+grep SHORTCUTURL ~/Documents/homepage/bookmarks.html \
+| sed -e 's/.*SHORTCUTURL=\"\(.*\).*$/\1/' \
+| sed -e 's/<\/A>$//g' \
+| sed -e 's/^/・/g' \
+| sort -u > ~/Documents/homepage/bookmarks-keywords.txt
+
+echo >> ~/Documents/homepage/bookmarks-keywords.txt
+echo "・[Titles]" >> ~/Documents/homepage/bookmarks-keywords.txt
+
+cat ~/Documents/homepage/bookmarks-title.txt >> ~/Documents/homepage/bookmarks-keywords.txt 
+
+scp -p ~/Documents/homepage/bookmarks-{keywords,title}.txt leaf@vaionote:~/www/localhost/homepage/
