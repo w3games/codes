c9844f847a9dd03444c48cdc3ebb5a416417892d
leaf <leaf@localhost.localdomain>
2016-03-09 15:53:23 +0900

    update script/bookmarks-keywords+title-generator.fish

--- script/bookmarks-keywords+title-generator.fish
+++ script/bookmarks-keywords+title-generator.fish
@@ -2,22 +2,34 @@
 #
 # bookmarks keywords+title generator
 
-grep \<DD\> ~/Documents/homepage/bookmarks.html \
-| sed -e 's/^[ ]*//g' \
-| sed -e 's/^<DD>/・/g' \
-| sort -u > ~/Documents/homepage/bookmarks-title.txt
+grep "<DD>" ~/Documents/homepage/bookmarks.html \
+	| sed -e 's/<[^>]*>//g' \
+	| sed -e 's/^$//g' \
+	| sed -e 's/^ *//g' \
+	| sed -e 's/^/・/g' \
+	| sort -u > ~/Documents/homepage/bookmarks-title.txt
+
+grep "<DT>" ~/Documents/homepage/bookmarks.html \
+	| sed -e 's/<[^>]*>//g' \
+	| sed -e 's/^$//g' \
+	| sed -e 's/^ *//g' \
+	| sed -e 's/^/・/g' \
+	| sort -u >> ~/Documents/homepage/bookmarks-title.txt
 
 echo "[Keywords]" > ~/Documents/homepage/bookmarks-keywords.txt
+
 grep SHORTCUTURL ~/Documents/homepage/bookmarks.html \
-| sed -e 's/.*SHORTCUTURL=\"\(.*\).*$/\1/' \
-| sed -e 's/<\/A>$//g' \
-| sed -e 's/^/・/g' \
-| sort -u >> ~/Documents/homepage/bookmarks-keywords.txt
+	| sed -e 's/<[^>]*>//g' \
+	| sed -e 's/^$//g' \
+	| sed -e 's/^ *//g' \
+	| sed -e 's/^/・/g' \
+	| sort -u >> ~/Documents/homepage/bookmarks-keywords.txt
 
 echo >> ~/Documents/homepage/bookmarks-keywords.txt
 echo "[Titles]" >> ~/Documents/homepage/bookmarks-keywords.txt
 
 cat ~/Documents/homepage/bookmarks-title.txt >> ~/Documents/homepage/bookmarks-keywords.txt 
+
 
 cp -p ~/Documents/homepage/bookmarks-{keywords,title}.txt /mnt/funtoo_586_stable/home/leaf/www/localhost/homepage/
 # scp -p ~/Documents/homepage/bookmarks-{keywords,title}.txt leaf@vaionote:~/www/localhost/homepage/
