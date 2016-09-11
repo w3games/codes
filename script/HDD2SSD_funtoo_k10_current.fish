9d69a0d7e823fa0edc3821154505f53e0eeeacbe
w3games <leaf@localhost.localdomain>
2016-09-11 17:17:02 +0900

    Add backup/restore var

--- script/HDD2SSD_funtoo_k10_current.fish
+++ script/HDD2SSD_funtoo_k10_current.fish
@@ -14,13 +14,13 @@ for c in SSD HDD
   end
 end
 mount -o defaults,compress=lz4,recovery,discard,ssd,inode_cache,space_cache,subvol=$DISTRI	LABEL="ssd"	$SSD
-# mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,ro,subvol=var	LABEL="various"	$SSD/var
+mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,ro,subvol=var	LABEL="various"	$SSD/var
 mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,ro,subvol=$DISTRI	LABEL="various"	$HDD
 
 rsync -ahAHSX --delete --info=progress2 --exclude etc/fstab $HDD/ $SSD/
 
 cp /home/leaf/fstab_ssd $SSD/etc/fstab
 
-umount $SSD $HDD
+umount $SSD $SSD/var $HDD
 rmdir  $SSD $HDD
 
--- script/SSD2HDD_funtoo_k10_current.fish
+++ script/SSD2HDD_funtoo_k10_current.fish
@@ -14,13 +14,13 @@ for c in SSD HDD
   end
 end
 mount -o defaults,compress=lz4,recovery,discard,ssd,inode_cache,space_cache,subvol=$DISTRI	LABEL="ssd"	$SSD
-# mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,subvol=var		LABEL="various"	$SSD/var
+mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,subvol=var		LABEL="various"	$SSD/var
 mount -o defaults,compress=lz4,recovery,autodefrag,inode_cache,space_cache,subvol=$DISTRI	LABEL="various"	$HDD
 
 rsync -ahAHSX --delete --info=progress2 --exclude etc/fstab $SSD/ $HDD/
 
 cp $SSD/etc/fstab /home/leaf/fstab_ssd
 
-umount $SSD $HDD
+umount $SSD $SSD/var $HDD
 rmdir  $SSD $HDD
 
