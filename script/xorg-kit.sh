599f385c232aec97dde76e1d7531bd6a3f21140d
w3games <leaf@localhost.localdomain>
2017-04-17 05:35:13 +0900

    Remove funtoo git mirror

--- script/xorg-kit.sh
+++ script/xorg-kit.sh
@@ -2,16 +2,18 @@
 #
 # Git Fetch from Funtoo Portage Tree to Local Mirror
 
-cd /var/git/funtoo/xorg-kit.git
+# cd /var/git/funtoo/xorg-kit.git
+cd /var/lib/portage/funtoo/xorg-kit
 
 # Get HEAD Commits
-LOCAL_COMMIT=`su nobody -s "/bin/sh" -c "git --no-pager log --pretty=format:%H -1"`
-REMOTE_COMMIT=`su nobody -s "/bin/sh" -c "git ls-remote origin HEAD" | cut -f 1`
+LOCAL_COMMIT=`"/bin/sh" -c "git --no-pager log --pretty=format:%H -1"`
+REMOTE_COMMIT=`"/bin/sh" -c "git ls-remote origin HEAD" | cut -f 1`
 
 # Main
 if [ $LOCAL_COMMIT != $REMOTE_COMMIT ]
   then
-    su nobody -s "/bin/sh" -c "git fetch"
+    echo "New ebuild found."
+    exit 0
   else
     echo "Already up-to-date."
     exit 1
