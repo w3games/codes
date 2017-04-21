20c075891194ad8ff847d3d1a3b08ad440589336
w3games <leaf@localhost.localdomain>
2017-04-21 16:03:01 +0900

    Update ports-2012.sh

--- script/ports-2012.sh
+++ script/ports-2012.sh
@@ -2,7 +2,8 @@
 #
 # Git Fetch from Funtoo Portage Tree to Local Mirror
 
-cd /var/git/funtoo/ports-2012.git
+# cd /var/git/funtoo/ports-2012.git
+cd /usr/portage
 
 # Get HEAD Commits
 LOCAL_COMMIT=`su nobody -s "/bin/sh" -c "git log --pretty=format:%H -1"`
@@ -11,7 +12,8 @@ REMOTE_COMMIT=`su nobody -s "/bin/sh" -c "git ls-remote origin HEAD" | cut -f 1`
 # Main
 if [ $LOCAL_COMMIT != $REMOTE_COMMIT ]
   then
-    su nobody -s "/bin/sh" -c "git fetch"
+    # su nobody -s "/bin/sh" -c "git fetch"
+    echo "New ebuilds arrived."
   else
     echo "Already up-to-date."
     exit 1
