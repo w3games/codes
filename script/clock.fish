3b0e00ac564e94b4636d6c9f8cad74967eddee59
w3games <leaf@localhost.localdomain>
2017-12-20 14:00:34 +0900

    Change order for left side

--- script/clock.fish
+++ script/clock.fish
@@ -6,7 +6,8 @@ set LINE (math (tput lines)-1)
 set COL  (math (tput cols)-1)
 
 while tput cup $LINE 0
-  echo -n  (cat /proc/loadavg | cut -d ' ' -f1,2,3) " " (date +"%H:%M.%S   %F   %A ")
+  # echo -n  (cat /proc/loadavg | cut -d ' ' -f1,2,3) " " (date +"%H:%M.%S   %F   %A ")
+  echo -n  (date +" %A  %F  %H:%M.%S ")  (cat /proc/loadavg | cut -d ' ' -f1,2,3)
   tput cup $LINE $COL
   sleep 1
 end
