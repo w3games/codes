b088e3e8c29ec7abdd386e46d5dd8b97b74bc367
leaf <leaf@localhost.localdomain>
2018-05-29 17:44:46 +0900

    Add clear to wipe out terminal

--- script/clock.fish
+++ script/clock.fish
@@ -5,6 +5,8 @@
 set LINE (math (tput lines)-1)
 set COL  (math (tput cols)-1)
 
+clear
+
 while tput cup $LINE 0
   # echo -n  (cat /proc/loadavg | cut -d ' ' -f1,2,3) " " (date +"%H:%M.%S   %F   %A ")
   echo -n  (date +" %A  %F  %H:%M.%S ")  (cat /proc/loadavg | cut -d ' ' -f1,2,3)
