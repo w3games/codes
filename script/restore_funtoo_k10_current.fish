0ebf7480b246131a87a31e6c1d26264e908257e4
w3game <w3game@gmail.com>
2018-12-09 09:04:47 +0900

    Add restore funtoo_k10_current from backup to master

---
+++ script/restore_funtoo_k10_current.fish
@@ -0,0 +1,32 @@
+#!/bin/fish
+#
+# restore_funtoo_k10_current.fish
+
+set DISTRI funtoo
+set BACKUP /mnt/$DISTRI\_from
+set MASTER /mnt/$DISTRI\_to
+
+for c in MASTER BACKUP
+  if mountpoint -q $$c
+    umount $$c
+  else
+    mkdir -p $$c
+  end
+end
+mount -o defaults,compress=zstd,usebackuproot,autodefrag,inode_cache,space_cache,subvol=$DISTRI			LABEL="linux"	$MASTER
+mount -o defaults,compress=zstd,usebackuproot,autodefrag,inode_cache,space_cache,subvol=$DISTRI_k10_current,ro	LABEL="backup"	$BACKUP
+mount --rbind /boot $MASTER/boot
+
+echo "Syncing from BACKUP to MASTER." \n
+rsync 	 -ahAHSX \
+	 --delete \
+	 --info=progress2 \
+	 # --exclude etc/fstab \
+	 # --exclude var/tmp/ccache/ \
+	 $BACKUP /$MASTER/
+
+cp /home/leaf/fstab_original $MASTER/etc/fstab
+
+umount $MASTER/boot $MASTER $BACKUP
+rmdir  $MASTER $BACKUP
+
